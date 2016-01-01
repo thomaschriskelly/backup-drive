9d21c68802bca8248e15c6302d93ff911fd69a2d
thomaschriskelly <thomaschriskelly@gmail.com>
2016-01-01 11:08:17 -0500

    added initial backup script

---
+++ backup.cmd
@@ -0,0 +1,5 @@
+:: Backs up photos from the photo drive
+@echo Backing up the photo drive
+::/mir Mirrors a directory tree (equivalent to /e plus /purge)
+robocopy P:/Family_Photos B:/family_photos_backup /mir
+paus
